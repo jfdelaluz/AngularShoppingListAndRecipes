1026bd60e11235a8431983a158c273865930f77b
jesusdelaluz <jfdelaluz@gmail.com>
2018-11-23 16:08:00 -0700

    Added Recipe model and implemented it in recipes components

--- src/app/recipes/recipe_list/recipe_list.component.ts
+++ src/app/recipes/recipe_list/recipe_list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component ({
   selector: 'app-recipe_list',
   templateUrl: 'recipe_list.component.html',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
 })
 
 export class RecipeListComponent {
-  
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_010.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply another test',
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*'
+    )
+  ];
 }
