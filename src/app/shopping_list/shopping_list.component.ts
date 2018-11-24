3457bee22661ccdc03b1bf1b5bd470560ccf4578
jesusdelaluz <jfdelaluz@gmail.com>
2018-11-23 17:05:30 -0700

    Added Ingredient model and implementd it in shopping list section

--- src/app/shopping_list/shopping_list.component.ts
+++ src/app/shopping_list/shopping_list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping_list',
   templateUrl: './shopping_list.component.html',
@@ -7,5 +9,8 @@ import { Component } from '@angular/core';
 })
 
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 }
