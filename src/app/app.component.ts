5f4988c19f85930852e155761b59fb6bf6f068d0
jesusdelaluz <jfdelaluz@gmail.com>
2018-11-28 16:06:30 -0700

    Added menu navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  
+  activeSection: string = 'recipes';
+
+  setActiveSection(section: string) {
+    this.activeSection = section;
+  }
+
+  isRecipesSectionActive() {
+    return this.activeSection === 'recipes';
+  }
+
+  isShoppingListSectionActive() {
+    return this.activeSection === 'shopping-list';
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+  @Output() sectionChange = new EventEmitter<string>();
 
+  onRecipesSelected() {
+    this.sectionChange.emit('recipes');
+  }
+
+  onShoppingListSelected() {
+    this.sectionChange.emit('shopping-list');
+  }
 }
