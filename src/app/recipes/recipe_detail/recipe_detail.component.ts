9adeb3cec492b3d7a7c490689605f2b05c48a5ae
jesusdelaluz <jfdelaluz@gmail.com>
2018-12-10 19:09:44 -0700

    Added functionality to move ingredients from recipe to shopping list

--- src/app/recipes/recipe_detail/recipe_detail.component.ts
+++ src/app/recipes/recipe_detail/recipe_detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe_detail',
@@ -10,4 +11,10 @@ import { Recipe } from '../recipe.model';
 
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ingredientsToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
