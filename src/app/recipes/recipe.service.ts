745c551488289d8e1ff6c9a15077af565ff3aed6
jesusdelaluz <jfdelaluz@gmail.com>
2018-12-10 18:26:44 -0700

    Added ingredients model array into recipes model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,12 +10,20 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_010.jpg'
+      'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_010.jpg',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Pineapples', 2)
+      ]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply another test',
-      'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*'
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*',
+      [
+        new Ingredient('Salmon', 2),
+        new Ingredient('Butter', 1)
+      ]
     )
   ];
 
